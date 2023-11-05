28055a0e795d53606a204741ac6f2130bf7aea27
Beatriz Vago <beatriz@Rafaels-MacBook-Pro.local>
2023-11-05 09:49:50 -0300

    Iniciando o projeto + requisito 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,21 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product_test = Product(
+        "1",
+        "Chocolate",
+        "Lindt",
+        "02/11/2023",
+        "02/11/2025",
+        "111222333",
+        "Temperatura ambiente"
+    )
+
+    assert product_test.id == "1"
+    assert product_test.product_name == "Chocolate"
+    assert product_test.company_name == "Lindt"
+    assert product_test.manufacturing_date == "02/11/2023"
+    assert product_test.expiration_date == "02/11/2025"
+    assert product_test.serial_number == "111222333"
+    assert product_test.storage_instructions == "Temperatura ambiente"
