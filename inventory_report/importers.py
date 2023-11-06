094e6ac88f0612cdaa5f59ad9bbec2933501daea
Beatriz Vago <beatriz@Rafaels-MacBook-Pro.local>
2023-11-06 15:16:16 -0300

    Requisitos 3, 4 e 5

--- inventory_report/importers.py
+++ inventory_report/importers.py
@@ -1,12 +1,43 @@
+from abc import ABC, abstractmethod
+from inventory_report.product import Product
 from typing import Dict, Type
+import json
 
 
-class Importer:
-    pass
+class Importer(ABC):
+    # pass
+    def __init__(self, path: str) -> None:
+        self.path = path
+
+    @abstractmethod
+    def import_data(self) -> list[Product]:
+        pass
 
 
-class JsonImporter:
-    pass
+class JsonImporter(Importer):
+    # pass
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+
+    def import_data(self) -> list[Product]:
+        result = []
+
+        with open(self.path, 'r') as file:
+            data = json.load(file)
+
+        for product in data:
+            list_item = Product(
+                product["id"],
+                product["product_name"],
+                product["company_name"],
+                product["manufacturing_date"],
+                product["expiration_date"],
+                product["serial_number"],
+                product["storage_instructions"],
+            )
+            result.append(list_item)
+
+        return result
 
 
 class CsvImporter:
--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,18 @@
+from inventory_report.product import Product
+
+
 class Inventory:
-    pass
+    # pass
+    def __init__(self, data: list[Product]) -> None:
+        if data is not None:
+            return self.data
+        else:
+            self.data = []
+
+    @property
+    def data(self) -> list[Product]:
+        return self.data
+
+    def add_data(self, data: list[Product]) -> None:
+        for product in data:
+            return self.data.append(product)
