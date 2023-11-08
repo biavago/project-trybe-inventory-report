ce4b213bc7697952beac67d46231e4775bf587ee
Beatriz Vago <beatriz@Rafaels-MacBook-Pro.local>
2023-11-08 18:02:04 -0300

    Correção do req 05

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -2,17 +2,15 @@ from inventory_report.product import Product
 
 
 class Inventory:
-    # pass
     def __init__(self, data: list[Product] or None = None):
         if data is None:
-            return self.data
+            data = []
         else:
-            self.data = []
+            self.data = data
 
     @property
-    def data(self) -> list[Product]:
-        return self.data
+    def data(self):
+        self.data
 
     def add_data(self, data: list[Product]) -> None:
-        for product in data:
-            return self.data.append(product)
+        self.data += data
